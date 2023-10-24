b446720d4a46c3c8786d2db1ce3174d077bc33cc
Arsen <143072460+Arsen9988@users.noreply.github.com>
2023-10-24 11:30:52 +0200

    tenta tests

--- Exercises/test_ex/first_PY.py
+++ Exercises/test_ex/first_PY.py
@@ -6,6 +6,22 @@
 #     print('Feeeeeeel')
 
 
-from modules import square
+# from modules import square
 
-print(square(9))
+# print(square(9))
+
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+    
+
+num = 0
+look = []
+for y in range(10, 100):
+    for x in range(10, 100):
+        num = y * x
+        num = str(num)
+        if is_palindrome(num):
+            look.append(num)
+
+        
+print(look[-1])
